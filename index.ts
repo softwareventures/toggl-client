8b4cfcb7a086e5560abb3618d5f9b8a861ac0dcf
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-02-04 18:57:35 +0000

    feat(createclient): require the consumer to provide an implementation of fetch

--- index.ts
+++ index.ts
@@ -1,20 +1,23 @@
 import {resolve} from "url";
 
 export interface Client {
+    readonly fetch: typeof fetch;
     readonly mainEndpoint: string;
     readonly reportsEndpoint: string;
 }
 
 export interface ClientOptions {
+    readonly fetch: typeof fetch;
     readonly endpoint?: string;
     readonly mainEndpoint?: string;
     readonly reportsEndpoint?: string;
 }
 
-export function createClient(options?: ClientOptions): Client {
+export function createClient(options: ClientOptions): Client {
+    const fetch = options.fetch;
     const endpoint = options?.endpoint || "https://toggl.com/";
     const mainEndpoint = resolve(endpoint, options?.mainEndpoint || "api/v8");
     const reportsEndpoint = resolve(endpoint, options?.reportsEndpoint || "reports/api/v2");
 
-    return {mainEndpoint, reportsEndpoint};
+    return {fetch, mainEndpoint, reportsEndpoint};
 }
