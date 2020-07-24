19cb0cd1d019db7bad84e502f096dabc73eb79c8
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-07-25 00:35:22 +0100

    feat(project-user): add updateProjectUser function

--- toggl/project-user.ts
+++ toggl/project-user.ts
@@ -1,17 +1,23 @@
 import {AuthenticatedApiClient} from "./authentication";
 import {request, Response} from "./request-response";
 
-export interface ProjectUser extends CreateProjectUserOptions {
-    readonly id: number;
+export interface ProjectUser extends CreateProjectUserOptions, UpdateProjectUserOptions {
     readonly wid: number;
     readonly manager: boolean;
     readonly at: string;
 }
 
-export interface CreateProjectUserOptions {
+export interface CreateProjectUserOptions extends ProjectUserBase {
     readonly pid: number;
     readonly uid: number;
     readonly wid?: number;
+}
+
+export interface UpdateProjectUserOptions extends ProjectUserBase {
+    readonly id: number;
+}
+
+export interface ProjectUserBase {
     readonly manager?: boolean;
     readonly rate?: number;
 }
@@ -34,5 +40,16 @@ export async function getProjectUser(
     return request(client, {
         method: "GET",
         path: "project_users/" + projectUserId.toString(10)
+    }).then(response => response as Response<ProjectUser>);
+}
+
+export async function updateProjectUser(
+    client: AuthenticatedApiClient,
+    user: UpdateProjectUserOptions
+): Promise<Response<ProjectUser>> {
+    return request(client, {
+        method: "PUT",
+        path: "project_users/" + user.id.toString(10),
+        body: {project_user: user}
     }).then(response => response as Response<ProjectUser>);
 }
