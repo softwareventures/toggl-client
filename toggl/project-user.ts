83cd01191cc9c861da09d05d0cca3e6c93fdc996
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-07-25 02:03:55 +0100

    docs(project-user): document project-user types

--- toggl/project-user.ts
+++ toggl/project-user.ts
@@ -2,33 +2,44 @@ import {AuthenticatedApiClient} from "./authentication";
 import {request, Response} from "./request-response";
 
 export interface ProjectUser extends CreateProjectUserOptions, UpdateProjectUserOptions {
+    /** ID of the Workspace that the project belongs to. */
     readonly wid: number;
+    /** If true, the user has admin rights for this Project. */
     readonly manager: boolean;
+    /** Timestamp indicating when the Project User was last updated. */
     readonly at: string;
 }
 
 export interface CreateProjectUserOptions extends CreateProjectUserBaseOptions {
+    /** ID of the User who is a member of the Project. */
     readonly uid: number;
 }
 
 export interface CreateProjectUsersOptions extends CreateProjectUserBaseOptions {
+    /** IDs of the Users who are members of the Project. */
     readonly uids: readonly number[];
 }
 
 export interface CreateProjectUserBaseOptions extends ProjectUserBase {
+    /** ID of the Project this User is a member of. */
     readonly pid: number;
 }
 
 export interface UpdateProjectUserOptions extends ProjectUserBase {
+    /** Project User ID. */
     readonly id: number;
 }
 
 export interface UpdateProjectUsersOptions extends ProjectUserBase {
+    /** Project User IDs. */
     readonly ids: readonly number[];
 }
 
 export interface ProjectUserBase {
+    /** If true, the user has admin rights for this Project. */
     readonly manager?: boolean;
+    /** Hourly rate for the Project User, in the currency of the Project's
+     * Client, or in the default currency of the Workspace. */
     readonly rate?: number;
 }
 
