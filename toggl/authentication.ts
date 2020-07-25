fc606ac8e8a0ffa54e3a05f0cb90501074f99451
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-07-25 10:49:22 +0100

    feat(authentication): add signUpNewUser function

--- toggl/authentication.ts
+++ toggl/authentication.ts
@@ -27,6 +27,17 @@ export interface AuthenticatedApiClient extends ApiClient {
     readonly authorization: ApiToken;
 }
 
+export interface SignUpUserOptions {
+    /** A valid email address for the User. */
+    readonly email: string;
+    /** The User's password, at least six characters. */
+    readonly password: string;
+    /** IANA TZ timezone specifying the User's timezone. */
+    readonly timezone: string;
+    /** The name of the app used to sign the user up. */
+    readonly created_with: string;
+}
+
 export async function authenticate(
     client: ApiClient,
     authorization: Authorization
@@ -47,4 +58,18 @@ export async function resetApiToken(
     return request(client, {method: "POST", path: "reset_token"})
         .then(response => response as Response<string>)
         .then(mapResponse(apiToken => ({...client, authorization: {apiToken}})));
+}
+
+export async function signUpNewUser(
+    client: ApiClient,
+    options: SignUpUserOptions
+): Promise<Response<Authentication>> {
+    return request(client, {method: "POST", path: "signups", body: options})
+        .then(response => response as Response<User>)
+        .then(
+            mapResponse(user => ({
+                client: {...client, authorization: {apiToken: user.api_token}},
+                user
+            }))
+        );
 }
