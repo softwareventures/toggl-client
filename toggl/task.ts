ee511a50af2de6683e2159a38e2dd43fe658729f
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-07-30 18:58:17 +0100

    feat(task): add updateTask function

--- toggl/task.ts
+++ toggl/task.ts
@@ -7,9 +7,7 @@ import {request, Response} from "./request-response";
  * Available only for paid workspaces.
  */
 
-export interface Task extends CreateTaskOptions {
-    /** Task ID. */
-    readonly id: number;
+export interface Task extends CreateTaskOptions, UpdateTaskOptions {
     /** ID of the Workspace that this Task belongs to. */
     readonly wid: number;
     /** True if the Task is in progress, false if it is done. */
@@ -20,21 +18,31 @@ export interface Task extends CreateTaskOptions {
     readonly tracked_seconds: number;
 }
 
-export interface CreateTaskOptions {
-    /** Name of the Task. Must be unique within the associated Project. */
-    readonly name: string;
+export interface CreateTaskOptions extends UpdateTaskOptions {
     /** ID of the Project that this Task belongs to. */
     readonly pid: number;
     /** ID of the Workspace that this Task belongs to. */
     readonly wid?: number;
-    /** ID of the User this Task is assigned to. */
-    readonly uid?: number;
-    /** Estimated duration of the Task in seconds. */
-    readonly estimated_seconds?: number;
     /** True if the Task is in progress, false if it is done.
      *
      * @default true */
     readonly active?: boolean;
+}
+
+export interface UpdateTaskOptions extends TaskBase {
+    /** Task ID. */
+    readonly id: number;
+    /** ID of the User this Task is assigned to. */
+    readonly uid?: number;
+    /** Estimated duration of the Task in seconds. */
+    readonly estimated_seconds?: number;
+    /** True if the Task is in progress, false if it is done. */
+    readonly active?: boolean;
+}
+
+export interface TaskBase {
+    /** Name of the Task. Must be unique within the associated Project. */
+    readonly name: string;
 }
 
 export async function createTask(
@@ -55,5 +63,22 @@ export async function getTask(
     return request(client, {
         method: "GET",
         path: `tasks/${taskId}`
+    }).then(response => response as Response<Task>);
+}
+
+export async function updateTask(
+    client: AuthenticatedApiClient,
+    options: UpdateTaskOptions
+): Promise<Response<Task>> {
+    return request(client, {
+        method: "PUT",
+        path: `tasks/${options.id}`,
+        body: {
+            task: {
+                uid: options.uid,
+                estimated_seconds: options.estimated_seconds,
+                active: options.active
+            }
+        }
     }).then(response => response as Response<Task>);
 }
